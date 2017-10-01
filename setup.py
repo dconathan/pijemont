31a1ba4a7bafe7a62b05d4b3720db99607c39886
Liam <liam@cpan.org>
2017-10-01 15:45:40 -0500

    Make Pijemont into a distutils package

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name='pijemont',
+    author='Daniel Ross',
+    maintainer='Devin Conathan',
+    version='0.2',
+    packages=['pijemont'],
+    license='MIT',
+    long_description=open('README.md').read(),
+    install_requires=[
+        'ply==3.10',
+        'PyYAML==3.12',
+    ],
+)
